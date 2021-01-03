3aabd35f5cf32e023a74eed6443a9da7c2e1afd9
Luca Pizzamiglio <pizzamig@FreeBSD.org>
2021-01-03 21:39:39 +0100

    General output improvements
    commit 7a41d37a61826f21396b2914f620f7e8e34aecc9
    Author: Luca Pizzamiglio <pizzamig@FreeBSD.org>
    Date:   Sun Jan 3 21:33:16 2021 +0100
    
        Output depends now on the breathe cycle parameters
    
        Removed hard coded elements in the progression bars
    
    commit c04061a0392d9a8827b888768872b5c437fec089
    Author: Luca Pizzamiglio <pizzamig@FreeBSD.org>
    Date:   Sun Jan 3 19:18:22 2021 +0100
    
        Set 4-7-8 to 8 repetitions, instead of 5 minutes
    
    commit 59c21b2d73ca82be862f486d8636581b14ca1d9d
    Author: Luca Pizzamiglio <pizzamig@FreeBSD.org>
    Date:   Sun Jan 3 19:17:47 2021 +0100
    
        Changed progess bar style
    
        Using ====>----

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,26 @@ async fn breath(params: BreathSessionParams) {
     let mut session =
         breath::BreathingSession::new(&params.pattern, params.session_type, params.duration);
 
+    session.print_params();
     let multibar = indicatif::MultiProgress::new();
-    let pb = multibar.add(indicatif::ProgressBar::new(56));
-    pb.set_style(indicatif::ProgressStyle::default_bar().template("{spinner} {bar:40} {msg}"));
+    let pb = multibar.add(indicatif::ProgressBar::new(session.get_lengths_lcm()));
+    pb.set_style(
+        indicatif::ProgressStyle::default_bar()
+            .template("{spinner} {bar:40} {msg}")
+            .progress_chars("=>-"),
+    );
     pb.set_message(&session.get_phase_str());
-    let total = multibar.add(indicatif::ProgressBar::new(300));
+    let total = multibar.add(indicatif::ProgressBar::new(session.get_session_length()));
     total.set_style(
         indicatif::ProgressStyle::default_bar()
-            .template("{percent:>3}% {wide_bar} {elapsed_precise}"),
+            .template("{percent:>3}% {wide_bar} {elapsed_precise}")
+            .progress_chars("=>-"),
     );
     async_std::task::spawn(async move {
         multibar.join_and_clear().unwrap_or_default();
     });
     while interval.next().await.is_some() {
-        pb.inc(56 / session.get_current_phase_length());
+        pb.inc(session.get_lengths_lcm() / session.get_current_phase_length());
         total.inc(1);
         session.inc();
         if session.is_completed() {
